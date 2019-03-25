5c94f510d3025f26c536ae021cb68aaaa79df09f
dattebayoRob <robson.william65@gmail.com>
2019-03-25 20:54:16 -0300

    Initial Commit

---
+++ src/main/java/com/dtb/wsfsas/WsfsasApplication.java
@@ -0,0 +1,13 @@
+package com.dtb.wsfsas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsfsasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsfsasApplication.class, args);
+	}
+
+}
